9d483669dea994a6115a757344103fad69ee529c
yangaeyoung <aeyoung0325@naver.com>
2024-07-02 17:21:14 +0900

    styled-components

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,24 +1,43 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+// import styled from 'styled-components'
 
-function Detail (props) {
+// let YellowBtn = styled.button`
+//   background : ${props => props.bg};
+//   color : ${props => props.bg == 'blue' ? 'white' : 'black'};
+//   padding : 10px;
+// `
 
-let {id} = useParams();
-let findId = props.char.find(()=>{ x.id == id })
+function Detail(props) {
+
+  useEffect(()=>{
+  })
+  console.log('안녕')
+
+
+  let[count, setCount] = useState(0)
+
+  let { id } = useParams();
+  let 찾을상품 = props.char.find(function (x) {
+    return x.id == id
+  });
 
   return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <img src={props.char[id].img} width="80%" />
-          </div>
-          <div className="col-md-4">
-            <h4 className="pt-5">{props.char[id].title}</h4>
-            <p>{props.char[id].content}</p>
-            <p>{props.char[id].price}</p>
-            <button className="btn btn-danger">주문하기</button>
-          </div>
+    <div className="container">
+      {count}
+      <button onClick={()=>{ setCount(count+1) }}>버튼</button>
+      <div className="row">
+        <div className="col-md-6">
+          <img src={props.char[id].img} width="80%" />
+        </div>
+        <div className="col-md-4">
+          <h4 className="pt-5">{찾을상품.title}</h4>
+          <p>{찾을상품.content}</p>
+          <p>{찾을상품.price}</p>
+          <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+    </div>
   )
 }
 
